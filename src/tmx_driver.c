3afb2ed3c938aeeedb396e8a1824c1b74c7918fd
CyntexMore <comebackyt20@gmail.com>
2024-07-31 00:37:23 +0200

    fix: Fixed critical syntax error

--- src/tmx_driver.c
+++ src/tmx_driver.c
@@ -11,7 +11,8 @@
 // Kernel module information
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("CyntexMore");
-MODULE_DESCRIPTION("Linux kernel module for the Thrustmaster TMX Force Feedback racing wheel."");
+MODULE_DESCRIPTION("Linux kernel module for the Thrustmaster TMX Force "
+                   "Feedback racing wheel.");
 MODULE_VERSION("0.1");
 
 static const struct usb_device_id tmx_id_table[] = {
@@ -19,23 +20,23 @@ static const struct usb_device_id tmx_id_table[] = {
 MODULE_DEVICE_TABLE(usb, tmx_id_table);
 
 // Probe function
-static int tmx_probe(struct usb_interface *interface, const struct usb_device_id *id) {
+static int tmx_probe(struct usb_interface *interface,
+                     const struct usb_device_id *id) {
   printk(KERN_INFO "TMX Force Feedback Wheel connected\n");
 
   int value = 1;
 
-  struct usb_ctrlrequest packet = {
-      .bRequestType = 0x41,
-      .bRequest = 83,
-      .wValue = cpu_to_le16(value),
-      .wIndex = 0,
-      .wLength = 0
-  };
+  struct usb_ctrlrequest packet = {.bRequestType = 0x41,
+                                   .bRequest = 83,
+                                   .wValue = cpu_to_le16(value),
+                                   .wIndex = 0,
+                                   .wLength = 0};
 
   struct usb_device *device = interface_to_usbdev(iface);
   struct urb *tmxurb = usb_alloc_urb(0, GFP_KERNEL);
 
-  usb_fill_control_urb(tmxurb, device, usb_sndctrlpipe(device, 0), (unsigned char *)&packet, 0, 0, tmx_control_handler, 0);
+  usb_fill_control_urb(tmxurb, device, usb_sndctrlpipe(device, 0),
+                       (unsigned char *)&packet, 0, 0, tmx_control_handler, 0);
 
   usb_submit_urb(tmxurb, GFP_KERNEL);
 
